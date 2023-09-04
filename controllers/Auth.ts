8a0e4eff664f3db90f2e00f66e99563e8ae42a69
Sohail Khan <skhan@techathalon.com>
2023-09-04 16:10:41 +0530

    feat(backend): add userLogin APi

--- controllers/Auth.ts
+++ controllers/Auth.ts
@@ -1,50 +1,99 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import UserModel from "../models/User";
-import validator from 'validator';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const userRegister = async (request: Request, response: Response) => {
+  const { email, password, name } = request.body;
 
-const userRegister = async (request:Request,response:Response) => {
+  const user = await UserModel.findOne({ email: email });
 
-    const {email,password,name} = request.body;
+  if (user) {
+    return response.status(200).json("user has already registered");
+  }
+  if (!validator.isEmail(email)) {
+    console.log("invalid email");
+  }
 
-    const user = await UserModel.findOne({email:email});
+  if (
+    validator.isEmpty(password) ||
+    !validator.isLength(password, { min: 5 })
+  ) {
+    console.log("Password is to short");
+  }
+  bcrypt
+    .hash(password, 12)
+    .then((hashpw) => {
+      console.log(email);
 
-    if(user)
-    {
-        return response.status(200).json("user has already registered");
-    }
-    if (!validator.isEmail(email)) {
-        console.log('invalid email')
-    }
+      const user = new UserModel({
+        email: email,
+        password: hashpw,
+        name: name,
+      });
 
-    if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
-        console.log('Password is to short');
-    }
-    bcrypt.hash(password, 12).then(hashpw => {
-        console.log(email)
-
-        const user = new UserModel({
-            email: email,
-            password: hashpw,
-            name: name
-        })
-
-
-        return  user.save();
-    }).then(user => {
-        console.log(user);
-        if (user) {
-            const token = jwt.sign({
-                userId: user._id.toString(),
-                email: user.email
-            }, 'somesupersecretsecret', { expiresIn: '1h' });
-            return response.status(200).json({ email: user.email, lastName: user.lastName, name: user.name, token: token });
-        }
-    }).catch(error => {
-        console.log(error);
+      return user.save();
     })
-}
+    .then((user) => {
+      console.log(user);
+      if (user) {
+        const token = jwt.sign(
+          {
+            userId: user._id.toString(),
+            email: user.email,
+          },
+          "somesupersecretsecret",
+          { expiresIn: "1h" }
+        );
+        return response.status(200).json({
+          email: user.email,
+          lastName: user.lastName,
+          name: user.name,
+          token: token,
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+const loginUser = async (request: Request, response: Response) => {
+  const { email, password } = request.body;
 
-export {userRegister};
+  var loadUser:any;
+  const user = await UserModel.findOne({ email: email }).then((user) => {
+    loadUser = user;
+    if (!user) {
+        response.status(200).json("user not found");
+      }
+      else{
+        return bcrypt.compare(password,user?.password);
+      }
+     
+  }).then((isEqual) => {
+    if (!isEqual) {
+        const error = new Error("Password is not match");
+        
+        response.status(422).json({ message: error.message });
+        // throw error;
+    }
+    else {
+
+        const token = jwt.sign({
+            userId: loadUser._id.toString(),
+            email: loadUser.email
+        }, 'somesupersecretsecret');
+
+        return response.status(200).json({ email: loadUser.email, lastName: loadUser.lastName, name: loadUser.name, token: token });
+
+    }
+}).catch(error => {
+    console.log(error);
+})
+  
+  
+
+  
+};
+export { userRegister, loginUser };
